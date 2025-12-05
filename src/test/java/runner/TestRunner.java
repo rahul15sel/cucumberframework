c44ce8b4a0951d587e03eb9589b65475df143f75
testing.rahul@gmail.com <testing.rahul@gmail.com>
2025-12-05 13:14:39 -0800

    Updated for Allure Report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,16 +1,21 @@
 package runner;
 
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Listeners;
+import io.qameta.allure.testng.AllureTestNg;
 
+@Listeners({AllureTestNg.class})  // <-- Add this listener for Allure TestNG
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         plugin = {
                 "pretty",
                 "html:target/cucumber-report.html",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+                "json:target/cucumber-json/cucumber.json",  // Optional JSON output
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" // Allure plugin
         }
 )
-public class TestRunner extends AbstractTestNGCucumberTests {}
+public class TestRunner extends AbstractTestNGCucumberTests {
+    // Nothing else needed
+}
